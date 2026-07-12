19d748e600d32619e20f903d9cf7ded73fab574d
Meenakshi <meenakshisatpathy20@gmail.com>
2026-07-12 13:27:10 +0530

    phase 1

---
+++ src/main/java/com/engineer360/Engineer360Application.java
@@ -0,0 +1,13 @@
+package com.engineer360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Engineer360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Engineer360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/engineer360/Engineer360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineer360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Engineer360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
